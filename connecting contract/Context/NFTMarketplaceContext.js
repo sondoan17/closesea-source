f34c4f163e8ad1be725f54f71f09b0dcadb3e240
sondoan17 <sondoan17@gmail.com>
2024-08-08 18:12:21 +0700

    update

---
+++ connecting contract/Context/NFTMarketplaceContext.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useContext } from "react";
+import web3Modal from "web3modal";
+import { ethers } from "ethers";
+import Router from "next/router";
+
+//internal import
+import { NFTMarketplaceAddress, NFTMarketplaceABI } from "./constants";
+
+const NFTMarketplaceContext = () => {
+  return <div>NFTMarketplaceContext</div>;
+};
+
+export default NFTMarketplaceContext;
