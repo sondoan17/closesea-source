7015e5b551976c9754ab9cedaeb7b89f9a032c13
sondoan17 <sondoan17@gmail.com>
2024-08-10 01:26:31 +0700

    remove network and const vars

--- closesea/hardhat.config.js
+++ closesea/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+// const { vars } = require("hardhat/config");
+// const ALCHEMY_API_KEY = vars.get("nEXUTLTb9tJGiHkgDYToRKfvmZCoE8Z0");
+// const SEPOLIA_PRIVATE_KEY = vars.get("f03f4ea2156f3db54b2f25ca3f0e42bb4401dc26f9149d3dc6af9a9a604d42e3");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  // networks: {
+  //   sepolia: {
+  //     url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+  //     accounts: [SEPOLIA_PRIVATE_KEY]
+  //   }
+  // }
 };
