d1532c67b7215771be429eaa044531544a465c66
sondoan17 <sondoan17@gmail.com>
2024-08-12 03:11:57 +0700

    add search function

--- closesea/app/search/page.js
+++ closesea/app/search/page.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect, useContext } from "react";
 
 //internal import
 import { SearchBar } from "../../AllPages/Search/searchindex";
@@ -6,25 +7,59 @@ import { Slider, Brand, Filter } from "../../components/componentsindex";
 import { NFTCardTwo, Banner } from "../../AllPages/Collection/collectionindex";
 import images from "../../img";
 
+//import smart contract
+import { NFTMarketplaceContext } from "../../Context/NFTMarketplaceContext";
+
 const Search = () => {
-  const collectionArray = [
-    images.nft_image_1,
-    images.nft_image_2,
-    images.nft_image_8,
-    images.nft_image_6,
-    images.nft_image_5,
-    images.nft_image_9,
-    images.nft_image_7,
-    images.nft_image_4,
-    images.nft_image_3,
-  ];
+  const { fetchNFTs } = useContext(NFTMarketplaceContext);
+  const [nfts, setNfts] = useState([]);
+  const [nftsCopy, setNftsCopy] = useState([]);
+
+  useEffect(() => {
+    fetchNFTs()
+      .then((item) => {
+        setNfts(item.reverse());
+        setNftsCopy(item);
+      })
+      .catch((err) => {});
+  });
+
+  const onHandleSearch = (value) => {
+    const filteredNFTs = nfts.filter(({ name }) =>
+      name.toLowerCase().includes(value.toLowerCase())
+    );
+    if (filteredNFTs.length === 0) {
+      setNfts(nftsCopy);
+    } else {
+      setNfts(filteredNFTs);
+    }
+  };
+  const onClearSearch = () => {
+    if (nfts.length && nftsCopy.length) {
+      setNfts(nftsCopy);
+    }
+  };
+  // const collectionArray = [
+  //   images.nft_image_1,
+  //   images.nft_image_2,
+  //   images.nft_image_8,
+  //   images.nft_image_6,
+  //   images.nft_image_5,
+  //   images.nft_image_9,
+  //   images.nft_image_7,
+  //   images.nft_image_4,
+  //   images.nft_image_3,
+  // ];
 
   return (
     <>
       <Banner bannerImage={images.creatorbackground5} />
-      <SearchBar />
+      <SearchBar
+        onHandleSearch={onHandleSearch}
+        onClearSearch={onClearSearch}
+      />
       <Filter />
-      <NFTCardTwo NFTData={collectionArray} />
+      <NFTCardTwo NFTData={nfts} />
       <Slider />
       <Brand />
     </>
