3d92c5cf3a9728d27a3f800241953c4455243af7
sondoan17 <sondoan17@gmail.com>
2024-08-12 03:11:10 +0700

    extract data from URL param and send to /nft-details

--- closesea/app/nft-details/page.js
+++ closesea/app/nft-details/page.js
@@ -1,13 +1,40 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState, useContext } from "react";
+import { useSearchParams } from "next/navigation";
 
 //internal import
 import NFTDetails from "../../AllPages/NFTDetails/NFTDetails";
 import { Button, Category, Brand } from "../../components/componentsindex";
 
+//import smart contract data
+import { NFTMarketplaceContext } from "../../Context/NFTMarketplaceContext";
+
 const NFTDetailsPage = () => {
+  const { currentAccount } = useContext(NFTMarketplaceContext);
+  const [nft, setNft] = useState({
+    image: "",
+    tokenId: "",
+    name: "",
+    owner: "",
+    price: "",
+    seller: "",
+    description: "",
+  });
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    setNft({
+      image: searchParams.get("image"),
+      tokenId: searchParams.get("tokenId"),
+      name: searchParams.get("name"),
+      owner: searchParams.get("owner"),
+      price: searchParams.get("price"),
+      seller: searchParams.get("seller"),
+      description: searchParams.get("description"),
+    });
+  }, [searchParams]);
   return (
     <div>
-      <NFTDetails />
+      <NFTDetails nft={nft} />
       <Category />
       <Brand />
     </div>
