db4301fc103b786392f13cc71a4a445773606b26
sondoan17 <sondoan17@gmail.com>
2024-08-12 17:41:38 +0700

    create resell page

---
+++ closesea/app/resell-nft/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//internal import
+import Style from "../styles/Resell.module.css";
+
+const reSellToken = () => {
+  return <div>reSellToken</div>;
+};
+
+export default reSellToken;
