2f6675263ceefa4cb54687983160594a7ecde2c4
sondoan17 <sondoan17@gmail.com>
2024-08-15 02:15:53 +0700

    update

--- NFTAPI/app.js
+++ NFTAPI/app.js
@@ -1,6 +1,8 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   res.send("Hello");
@@ -18,6 +20,37 @@ const app = express();
 //     message: "your data",
 //   });
 // });
+
+//get request
+const nfts = JSON.parse(fs.readFileSync(`${__dirname}/data/nft-simple.json`));
+
+// console.log(nfts);
+
+app.get("/api/v1/nfts", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    result: nfts.length,
+    data: {
+      nfts,
+    },
+  });
+});
+
+app.post("/api/v1/nfts", (req, res) => {
+  const newId = nfts[nfts.length - 1].id + 1;
+  const newNFTs = Object.assign({ id: newId }, req.body);
+  nfts.push(newNFTs);
+  fs.writeFile(
+    `${__dirname}/data/nft-simple.json`,
+    JSON.stringify(nfts),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        nft: newNFTs,
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
--- closesea/Context/constants.js
+++ closesea/Context/constants.js
@@ -1,4 +1,5 @@
 //0x5FbDB2315678afecb367f032d93F642f64180aa3
+//0xd7864354d50f994dc710347F36Ab42C0EeE4e570
 
 import nftMarketplace from "./NFTMarketplace.json";
 
