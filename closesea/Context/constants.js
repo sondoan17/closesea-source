d20ff4641dbbc7188a037bf637e82ae706e9665a
sondoan17 <sondoan17@gmail.com>
2024-08-15 15:44:17 +0700

    testnet sepolia

--- closesea/Context/constants.js
+++ closesea/Context/constants.js
@@ -4,5 +4,5 @@
 import nftMarketplace from "./NFTMarketplace.json";
 
 export const NFTMarketplaceAddress =
-  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  "0xd7864354d50f994dc710347F36Ab42C0EeE4e570";
 export const NFTMarketplaceABI = nftMarketplace.abi;
