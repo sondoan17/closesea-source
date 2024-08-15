4c3cf1c7dad5e6de5d68e6fc959195ea918d82f3
sondoan17 <sondoan17@gmail.com>
2024-08-15 15:28:13 +0700

    update next config => set missingsuspend false

--- closesea/next.config.js
+++ closesea/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["gateway.pinata.cloud"],
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 module.exports = nextConfig;
